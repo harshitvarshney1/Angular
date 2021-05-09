802720756ae598901b78a1fbc40e9ed102fac10e
harshitvarshney1 <72073964+harshitvarshney1@users.noreply.github.com>
2021-05-09 21:15:11 +0530

    Delete FinalProject directory

--- FinalProject/src/app/product.ts
+++
@@ -1,9 +0,0 @@
-export interface Product {
-    id : number ,
-    title : string,
-    price : number,
-    quantity : number,
-    color : string,
-    expiryDate : string,
-    inStock : boolean
-}
